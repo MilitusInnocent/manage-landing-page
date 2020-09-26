0444a880851646b483b86dba78608f368b0ba40f
Militus <55919005+MilitusInnocent@users.noreply.github.com>
2020-09-26 04:11:42 +0100

    init

---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+export {default as Navbar} from './Navbar';
+export {default as Button} from './Button';
+export {default as Links} from './Links';
+export {default as Header} from './Header';
+export {default as Slider} from './Slider';
+export {default as SliderCard} from './SliderCard';
+export {default as About} from './About';
+export {default as AboutCard} from './AboutCard';
+export {default as CTA} from './CTA';
+export {default as Footer} from './Footer';
