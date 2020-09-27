0c0c9cc08992d8efc810b73b3157fa370716e7c3
Diarrah <diarrahcasiano@gmail.com>
2020-09-26 20:49:18 -0700

    Number prop added + implicit return

--- src/components/AboutCard.js
+++ src/components/AboutCard.js
@@ -1,12 +1,11 @@
 import React from 'react';
 
-const AboutCard = ({ heading, text}) => {
-    return (
-        <div className="about__card">
-            <h3 className="about__card__heading">{heading} </h3>
-            <p className="about__card__subheading">{text}</p>
-        </div>
-    )
-}
+const AboutCard = ({ number, heading, text }) => (
+    <div className="about__card">
+        <span className="about__card__number">{number}</span>
+        <h3 className="about__card__heading">{heading}</h3>
+        <p className="about__card__subheading">{text}</p>
+    </div>
+)
 
 export default AboutCard
