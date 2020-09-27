2ec7620ad3257437abcc18dded604a3f6477a07a
Militus <55919005+MilitusInnocent@users.noreply.github.com>
2020-09-27 01:27:51 +0100

    desktop navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import { Button, Links } from './';
+import companyLogo from '../images/logo.svg';
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <div>
+            <img className="navbar__logo" src={companyLogo} alt="company logo" onClick={closeAll} />
+            <div className="navbar__links">
+                <Links/>
+            </div>
+            <Button/>
+        </div>
+        
     )
 }
 
