881f5ec106c989d9e7b195bb0513a0ca00adc7c4
Diarrah <diarrahcasiano@gmail.com>
2020-09-28 21:27:03 -0700

    Added className to header + number parent div

--- src/components/AboutCard.js
+++ src/components/AboutCard.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const AboutCard = ({ number, heading, text }) => (
     <div className="about__card">
-        <div style={{ display: "flex" }}>
+        <div className="about__card__top" style={{ display: "flex" }}>
             <span className="about__card__number">{number}</span>
             <h3 className="about__card__heading">{heading}</h3>
         </div>
